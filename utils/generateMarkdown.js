5399d8920bfe6722f63705a916f9ebe094f16783
Eoin Harkin <eharky@gmail.com>
2022-01-07 12:34:43 +0000

    started the markup using temp litreals

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,12 +12,15 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return license;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${data.projectName}
+  ${renderLicenseBadge(data.license)}
+  ${renderLicenseLink(data.license)}
 `;
 }
 
